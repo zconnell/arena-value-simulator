ab2dfab156e078e42ddcd775fe56d1c8ed99d0c6
Zach Connell <67717568+zconnell@users.noreply.github.com>
2020-07-02 17:34:00 -0500

    Updated ArenaValueSimulator
    Added comments, minor clean up

--- ArenaValueSimulator.py
+++ ArenaValueSimulator.py
@@ -3,22 +3,30 @@ from ArenaSimulation import ArenaSimulation
 import numpy
 import json
 
+# Create the Best of 1 (BO1) and Best of 3 (BO3) events
 prizeBO1 = {"gems":[50, 100, 250, 1000, 1400, 1600, 1800, 2200],
              "packs":[1,1,2,2,3,4,5,6]}
-eventBO1 = ArenaEvent("B01",7,3,10,1,prizeBO1,1500)
-
+BO1 = ArenaEvent("BO1",7,3,10,1,prizeBO1,1500)
 prizeBO3 = {"gems":[0,0,1000,3000],
              "packs":[1,1,4,6]}
-eventBO3 = ArenaEvent("B03",3,3,3,3,prizeBO3,1500)
-eventTypes = [eventBO1,eventBO3]
+BO3 = ArenaEvent("BO3",3,3,3,3,prizeBO3,1500)
 
-ID = 0
-simulations = {}
-for thisEvent in eventTypes:
-    for winPercent in numpy.arange(0.4,0.8,0.01):
-        startingGems = 20000
+# All events to be simulated to be included in the "events" list
+events = [BO1,BO3]
+
+# Initialize values for the simulation
+simulations = {} # simulations dictionary collects all simulations performed
+startingGems = 20000 # Change value to change the number of gems for all starting simulations
+simsPerWinPercent = 250 # Change value to change the number of sims performed for each win Percentage
+minWinPercent = 0.4 # Change value to change the minimum win percentage simulated
+maxWinPercent = 0.75 # Change value to change the maximum win percentage simulated
+stepWinPercent = 0.005 # Change value to change the step between win percentages
+
+for thisEvent in events:
+    for winPercent in numpy.arange(minWinPercent,maxWinPercent,stepWinPercent):
+        ID = thisEvent.name + "-" + str(winPercent)
         thisSim = ArenaSimulation(str(ID),winPercent,startingGems,thisEvent)
-        for ii in range(10):
+        for ii in range(simsPerWinPercent):
             thisSim.ID = str(ID) + "-" + str(ii)
             thisSim.runSim()
             simulations[thisSim.ID] = {
@@ -31,6 +39,7 @@ for thisEvent in eventTypes:
                 "packs won": thisSim.packHistory[-1],
                 "pack history": thisSim.packHistory
                 }
-        ID += 1
 
-json.dump(simulations,open("simulations1.json","w"))
+outfile = "simulations.json" # Change value to change the JSON output file name
+
+json.dump(simulations,open(outfile,"w"))
